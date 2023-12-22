6949a7b5498dfe944bedcbb5e8469bd8453daab4
Alicecaglisse01 <alice0404.moisan@gmail.com>
2023-12-22 14:07:32 +0100

    deuxieme seance

--- Angular/angular-learning/projects/todolist/src/app/app.component.spec.ts
+++ Angular/angular-learning/projects/todolist/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'sandbox' title`, () => {
+  it(`should have the 'todolist' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('sandbox');
+    expect(app.title).toEqual('todolist');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, sandbox');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, todolist');
   });
 });
